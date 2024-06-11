218648e11448cf2525a57f4d4e95d88123ae4e36
TeodorK0907 <ted_kehayov@abv.bg>
2024-06-11 16:05:04 +0300

    Update CsvWriter and HtmlWriter to no longer rely on studentPins

--- src/writers/CsvWriter.java
+++ src/writers/CsvWriter.java
@@ -10,78 +10,51 @@ import java.util.List;
 
 public class CsvWriter implements Writer {
     private static final String CSV_SEPARATOR = ",";
+    private static final String CSV_FILE_NAME = "report.csv";
 
+    @Override
     public void write(String fileDir,
-                           List<StudentReport> studentReports,
-                           List<CourseReport> courseReports,
-                           List<String> studentPins) {
+                      List<StudentReport> studentReports,
+                      List<CourseReport> courseReports) {
         try {
             File dir = new File(fileDir);
             dir.mkdir();
-            File file = new File(dir, "report.csv");
+            File file = new File(dir, CSV_FILE_NAME);
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8));
             StringBuilder headers = new StringBuilder();
             populateHeaders(headers, bw);
-            if (!studentPins.isEmpty()) {
-                for (String pin : studentPins) {
-                    StringBuilder content = new StringBuilder();
-                    for (StudentReport studentReport : studentReports) {
-                        if (pin.equals(studentReport.getPin())) {
-                            content.append(studentReport.getStudentNames());
-                            content.append(CSV_SEPARATOR);
-                            content.append(studentReport.getTotalCredits());
-                            content.append(CSV_SEPARATOR);
-                            bw.write(content.toString());
-                            bw.newLine();
-                        }
-                        for (CourseReport courseReport : courseReports) {
-                            if (pin.equals(courseReport.getPin())) {
-                                String courseContent = "" +
-                                        CSV_SEPARATOR +
-                                        courseReport.getCourseName() +
-                                        CSV_SEPARATOR +
-                                        courseReport.getTotalTime() +
-                                        CSV_SEPARATOR +
-                                        courseReport.getCredit() +
-                                        CSV_SEPARATOR +
-                                        courseReport.getInstructorNames();
-                                bw.write(courseContent);
-                                bw.newLine();
-                            }
-                        }
-                    }
-                }
-            } else {
-                for (StudentReport studentReport : studentReports) {
-                    StringBuilder content = new StringBuilder();
-                    content.append(studentReport.getStudentNames());
-                    content.append(CSV_SEPARATOR);
-                    content.append(studentReport.getTotalCredits());
-                    content.append(CSV_SEPARATOR);
-                    bw.write(content.toString());
-                    bw.newLine();
-                    for (CourseReport courseReport : courseReports) {
-                        if (courseReport.getPin().equals(studentReport.getPin())) {
-                            String courseContent = "" +
-                                    CSV_SEPARATOR +
-                                    courseReport.getCourseName() +
-                                    CSV_SEPARATOR +
-                                    courseReport.getTotalTime() +
-                                    CSV_SEPARATOR +
-                                    courseReport.getCredit() +
-                                    CSV_SEPARATOR +
-                                    courseReport.getInstructorNames();
-                            bw.write(courseContent);
-                            bw.newLine();
-                        }
+            for (StudentReport studentReport : studentReports) {
+                StringBuilder content = new StringBuilder();
+                content.append(studentReport.getStudentNames());
+                content.append(CSV_SEPARATOR);
+                content.append(studentReport.getTotalCredits());
+                content.append(CSV_SEPARATOR);
+                bw.write(content.toString());
+                bw.newLine();
+                for (CourseReport courseReport : courseReports) {
+                    if (studentReport.getPin().equals(courseReport.getPin())) {
+                        String courseContent = "" +
+                                CSV_SEPARATOR +
+                                courseReport.getCourseName() +
+                                CSV_SEPARATOR +
+                                courseReport.getTotalTime() +
+                                CSV_SEPARATOR +
+                                courseReport.getCredit() +
+                                CSV_SEPARATOR +
+                                courseReport.getInstructorNames();
+                        bw.write(courseContent);
+                        bw.newLine();
                     }
                 }
             }
             bw.flush();
             bw.close();
         } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
